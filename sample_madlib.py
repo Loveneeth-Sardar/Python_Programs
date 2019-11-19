182ff4ab1a058e3020d1242cd0c83e22df0a04eb
Loveneeth-Sardar <57132671+Loveneeth-Sardar@users.noreply.github.com>
2019-11-19 14:12:42 +0530

    Add files via upload

---
+++ sample_madlib.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 30 15:23:25 2019
+
+@author: user
+"""
+
+print('Lets play a game !')
+
+input_1 =input("Animal name: ").lower()
+input_2=input("Animal food: ").lower()
+input_3=input("City name: ").lower()
+print('Once upon a time, deep in an ancient jungle,there lived a \
+'+ str(input_1)+'.This ' +str(input_1) +' liked to eat '+ str(input_2)+'\
+but the jungle had very little '+str(input_2) +' to offer.\
+One day, an explorer found the '+str(input_1) +' and discovered it liked \
+'+str(input_2) +' The explorer took the '+str(input_1) +' back to \
+'+str(input_3) +' where it could eat as much '+str(input_2) +'\
+it wanted.However,the '+str(input_1) +'could eat as much '+str(input_2) +'as \
+it wanted.However,the '+str(input_1) +' became homesick, so the explorer brought \
+it back to the jungle,leaving a large supply of '+str(input_2))
